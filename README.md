6f33d15a40c6a700ade2e723e3d0eae610581477
Jack <jackgarbus@gmail.com>
2023-10-14 18:18:54 -0400

    init to commit it

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # XPlot
 
+A plotting program for plotting and comparing aggregate experimental data in the terminal
+
 [![Build Status](https://github.com/jarbus/XPlot.jl/actions/workflows/CI.yml/badge.svg?branch=master)](https://github.com/jarbus/XPlot.jl/actions/workflows/CI.yml?query=branch%3Amaster)
+
+
+* needs to handle plotting multiple types of data in multiple plots
+* needs to support plotting a single data source
+* needs to support aggregation of n data sources
+* needs to support plotting n data sources in n subplots
--- src/XPlot.jl
+++ src/XPlot.jl
@@ -1,5 +1,65 @@
 module XPlot
 
-# Write your package code here.
+using JLD2
+abstract type AbstractTimeSeriesDataPoint end
+abstract type AbstractTimeSeries end
+
+struct TimeSeriesDataPoint <: AbstractTimeSeriesDataPoint
+    x::Float64
+    value::Float64
+    upper_bound::Union{Float64, Nothing}
+    lower_bound::Union{Float64, Nothing}
+end
+
+struct AggregatedTimeSeriesDataPoint <: AbstractTimeSeriesDataPoint
+    x::Float64
+    value::Float64
+    upper_bound::Union{Float64, Nothing}
+    lower_bound::Union{Float64, Nothing}
+    count::Int64
+end
+
+struct TimeSeriesData <: AbstractTimeSeries
+    name::String
+    data::Vector{TimeSeriesDataPoint}
+end
+
+struct TimeSeriesPlot
+    name::String
+    data::Vector{TimeSeriesData}
+end
+
+struct InteractionDistanceError <: AbstractTimeSeries
+    distance::Int
+    data::Vector{TimeSeriesDataPoint}
+end
+
+function InteractionDistanceErrors(
+        distances::Vector{Int},
+        path::String
+    )
+    datapoints = Dict{Int, InteractionDistanceError}(
+            d => InteractionDistanceError(d,[]) for d in distances)
+
+    jldopen(path, "r") do file
+        for gen in keys(file["gen"])
+            for distance in distances
+                stats = file["gen/$gen/tree_stats/dist_int_errors/$distance/"] 
+                datapoint = TimeSeriesDataPoint(
+                    parse(Int, gen),
+                    stats["mean"],
+                    stats["upper_confidence"],
+                    stats["lower_confidence"]
+                )
+                push!(datapoints[distance].data, datapoint)
+            end
+        end
+    end
+end
+
+
+
+
+
 
 end
