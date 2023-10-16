18fe3d01ca04f9f1281fd447f74f87316a389aa8
Jack <jackgarbus@gmail.com>
2023-10-16 12:45:10 -0400

    mv: test data structure

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,16 +1,17 @@
 using XPlot
 using Plots
 using Test
-data = joinpath(@__DIR__, "data")
-@testset "XPlot.jl" begin
-    # Write your tests here.
+@testset "InteractionDistanceErrors" begin
+    data = joinpath(@__DIR__, "x/interaction-distance-1/data")
+    # We probably want to move this into PhyloCoEvo at some point
     iderrs = XPlot.InteractionDistanceErrors([1,2,3], joinpath(data, "archive.jld2"))
     @test length(iderrs) == 3
     tsp = XPlot.TimeSeriesPlot("test", collect(values(iderrs)))
     XPlot.plot(tsp)
     savefig("test.png")
-    run(`kitten icat test.png`)
     # clear current plot
     plot()
-    # run(`rm test.png`)
+    run(`rm test.png`)
+end
+
 end
