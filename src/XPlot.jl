21a80b63065fd9e4f62312f05d1e3b141535ed35
Jack <jackgarbus@gmail.com>
2023-10-16 15:25:13 -0400

    feat: add AggredatedTimeSeries{Data,DataPoint}, agg()

--- src/XPlot.jl
+++ src/XPlot.jl
@@ -1,5 +1,7 @@
 module XPlot
 using JLD2
+using StatsBase
+using HypothesisTests
 using Plots
 abstract type AbstractTimeSeriesDataPoint end
 abstract type AbstractTimeSeries end
@@ -7,18 +9,31 @@ abstract type AbstractTimeSeries end
 struct TimeSeriesDataPoint <: AbstractTimeSeriesDataPoint
     x::Float64
     value::Float64
-    upper_bound::Union{Float64, Nothing}
     lower_bound::Union{Float64, Nothing}
+    upper_bound::Union{Float64, Nothing}
 end
 
-Base.show(io::IO, datapoint::TimeSeriesDataPoint) = print(io, "($(round(datapoint.x, digits=2)), $(round(datapoint.value, digits=2)))")
+Base.show(io::IO, datapoint::AbstractTimeSeriesDataPoint) = print(io, "($(round(datapoint.x, digits=2)), $(round(datapoint.value, digits=2)))")
 
 struct AggregatedTimeSeriesDataPoint <: AbstractTimeSeriesDataPoint
     x::Float64
     value::Float64
-    upper_bound::Union{Float64, Nothing}
-    lower_bound::Union{Float64, Nothing}
+    lower_bound::Float64
+    upper_bound::Float64
     count::Int64
+end
+
+function AggregatedTimeSeriesDataPoint(datapoints::Vector{TimeSeriesDataPoint})
+    # check that all xs are the same
+    @assert length(unique([d.x for d in datapoints])) == 1
+    x = datapoints[1].x
+
+    vs = [d.value for d in datapoints]
+    value = mean(vs)
+    test = OneSampleTTest(vs)
+    upper_bound, lower_bound = confint(test)
+    count = length(datapoints)
+    return AggregatedTimeSeriesDataPoint(x, value, lower_bound, upper_bound, count)
 end
 
 struct TimeSeriesData <: AbstractTimeSeries
@@ -26,13 +41,16 @@ struct TimeSeriesData <: AbstractTimeSeries
     data::Vector{AbstractTimeSeriesDataPoint}
 end
 
+struct AggregatedTimeSeriesData <: AbstractTimeSeries
+    name::String
+    data::Vector{AggregatedTimeSeriesDataPoint}
+end
+
 function plot(p::AbstractTimeSeries)
     xs = [d.x for d in p.data]
     ys = [d.value for d in p.data]
     upper = [d.upper_bound - d.value for d in p.data]
-    println(upper)
     lower = [d.value - d.lower_bound for d in p.data]
-    println(lower)
     plot!(xs, ys, ribbon=(upper, lower), label=p.name)
 end
 
@@ -64,15 +82,50 @@ function InteractionDistanceErrors(
                 datapoint = TimeSeriesDataPoint(
                     parse(Int, gen),
                     stats["mean"],
+                    stats["lower_confidence"],
                     stats["upper_confidence"],
-                    stats["lower_confidence"]
                 )
                 push!(datapoints[distance].data, datapoint)
             end
         end
     end
     datapoints = filter(x -> length(x[2].data) > 0, datapoints)
+    # sort datapoints by x
+    for (distance, iderr) in datapoints
+        sort!(iderr.data, by=x->x.x)
+    end
     return datapoints
+end
+
+function AggregatedTimeSeriesData(
+    name::String,
+    data::Vector{TimeSeriesData}
+)
+    # assert all time series data have the same name
+    @assert length(unique([d.name for d in data])) == 1
+    agg_data = Dict{Float64, Vector{TimeSeriesDataPoint}}()
+    # aggregate data over time series
+    for tsd in data
+        for datapoint in tsd.data
+            if !haskey(agg_data, datapoint.x)
+                agg_data[datapoint.x] = []
+            end
+            push!(agg_data[datapoint.x], datapoint)
+        end
+    end
+    # aggregate data over time points
+    xs = sort!(collect(keys(agg_data)))
+    datapoints = [AggregatedTimeSeriesDataPoint(agg_data[x]) for x in xs]
+    agg_data = AggregatedTimeSeriesData(name, datapoints)
+    agg_data
+end
+
+function agg(timeseriesdata::Vector{TimeSeriesData})
+    agg_data = Dict{String, Vector{TimeSeriesData}}(tsd.name => [] for tsd in timeseriesdata)
+    for tsd in timeseriesdata
+        push!(agg_data[tsd.name], tsd)
+    end
+    return [AggregatedTimeSeriesData(name, data) for (name, data) in agg_data]
 end
 
 function plot(p::TimeSeriesPlot)
--- test/runtests.jl
+++ test/runtests.jl
@@ -14,4 +14,51 @@ using Test
     run(`rm test.png`)
 end
 
+@testset "DummyData" begin
+    TSDP = XPlot.TimeSeriesDataPoint
+    tsd1a = XPlot.TimeSeriesData("dummy-data-1", [TSDP(1, 1, 0.5, 1.5), TSDP(2, 2, 1, 3), TSDP(3,2,1,3)])
+    tsd1b = XPlot.TimeSeriesData("dummy-data-1", [TSDP(1, 5, 4.5, 5.5), TSDP(2, 6, 5, 7), TSDP(3,6,5,7)])
+    tsd2 = XPlot.TimeSeriesData("dummy-data-2", [TSDP(1, 5, 4.5, 5.5), TSDP(2, 6, 5, 7), TSDP(3,6,5,7)])
+    @testset "TimeSeriesPlot" begin
+        # Plot two different time series
+        tsp = XPlot.TimeSeriesPlot("test", [tsd1a, tsd2])
+        XPlot.plot(tsp)
+        figname = joinpath(@__DIR__, "dummy-figs/dummy-data-1a,2.png")
+        savefig(figname)
+        @test length(tsp.data) == 2
+        @test isfile(figname)
+        # clear current plot
+        plot()
+    end
+    @testset "Aggregation" begin
+        # Create aggregate time series data
+        figname = joinpath(@__DIR__, "dummy-figs/agg-dummy-data-1a,1b.png")
+        manual_agg_data = XPlot.AggregatedTimeSeriesData("agg_dummy_data", [tsd1a, tsd1b])
+        n_samples = 10
+        dd1 = repeat([tsd1a, tsd1b], Int(n_samples / 2))
+        dd2 = repeat([tsd2], n_samples)
+        auto_agg_data = XPlot.agg(vcat(dd1, dd2))
+        @test length(auto_agg_data) == 2
+        names = [d.name for d in auto_agg_data]
+        @test "dummy-data-1" ∈ names
+        @test "dummy-data-2" ∈ names
+        agg_dd1 = auto_agg_data[findfirst(x -> x.name == "dummy-data-1", auto_agg_data)]
+        agg_dd2 = auto_agg_data[findfirst(x -> x.name == "dummy-data-2", auto_agg_data)]
+        # verify aggregate data for dummy-data-1
+        @test length(agg_dd1.data) == 3
+        @test agg_dd1.data[1].x == 1 && agg_dd1.data[1].value == 3 && agg_dd1.data[1].count == n_samples
+        @test agg_dd1.data[2].x == 2 && agg_dd1.data[2].value == 4 && agg_dd1.data[2].count == n_samples
+        @test agg_dd1.data[3].x == 3 && agg_dd1.data[3].value == 4 && agg_dd1.data[3].count == n_samples
+
+        # verify aggregate data for dummy-data-2
+        @test length(agg_dd2.data) == 3
+        @test agg_dd2.data[1].x == 1 && agg_dd2.data[1].value == 5 && agg_dd2.data[1].count == n_samples
+        @test agg_dd2.data[2].x == 2 && agg_dd2.data[2].value == 6 && agg_dd2.data[2].count == n_samples
+        @test agg_dd2.data[3].x == 3 && agg_dd2.data[3].value == 6 && agg_dd2.data[3].count == n_samples
+        XPlot.plot(XPlot.TimeSeriesPlot("test", [agg_dd1]))
+        savefig(figname)
+        @test isfile(figname)
+        # clear current plot
+        plot()
+    end
 end
