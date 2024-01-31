818d6736899273bbfc4a5c1bef1158e0324cdfcb
jarbus <jarbus@tutanota.com>
2024-01-30 19:52:33 -0500

    fix: handle time series of different lengths

--- src/metrics.jl
+++ src/metrics.jl
@@ -3,7 +3,16 @@ abstract type AbstractMetric end
 
 function get_species_matchups(file, metric="phylogeneticestimatorstats"; head="gen")
     # get the largest number string
-    max_gen = keys(file[head]) |> collect |> x->sort(x, by=y->parse(Int, y)) |> last
+    gens = keys(file[head]) |> collect |> x->sort(x, by=y->parse(Int, y), rev=true)
+    # from the end of the list to the beginning, find the first generation
+    # that has the key "$head/$gen/$metric"
+    max_gen = 1
+    for gen in gens
+        if haskey(file, "$head/$gen/$metric")
+            max_gen = gen
+            break
+        end
+    end
     # get all species in the last generation
     species = keys(file["$head/$max_gen/$metric"]) |> collect
     species
